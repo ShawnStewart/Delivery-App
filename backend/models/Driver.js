a054fd3adaade0d9cecdcc259bb0cf6360fa1a9b
Alec Richardson <alec.richardson@mail.missouri.edu>
2018-09-09 22:01:46 -0500

    Customer and Driver model

--- backend/models/Driver.js
+++ backend/models/Driver.js
@@ -2,6 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
-const DriverSchema = new Schema({});
+const DriverSchema = new Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  vehicle: {
+    year: { type: Number, required: true },
+    make: { type: String, required: true },
+    model: { type: String, required: true }
+  },
+  orders: [
+    {
+      type: ObjectId,
+      ref: "orders"
+    }
+  ]
+});
 
 module.exports = Driver = mongoose.model("drivers", DriverSchema);
