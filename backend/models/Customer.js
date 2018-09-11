ac1afef884d634d743b2deb80ac70dddfe6014ae
Shawn Stewart <s.stewart0220@gmail.com>
2018-09-11 16:25:52 -0400

    Formatting changes

--- backend/models/Customer.js
+++ backend/models/Customer.js
@@ -1,13 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
-//Customer
+
 const CustomerSchema = new Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
-  phone: { type: Number, required: true },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: Number,
+    required: true
+  },
   birthday: Date,
   address: {
     street: String,
--- backend/models/Driver.js
+++ backend/models/Driver.js
@@ -1,17 +1,37 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
-//Driver
 
 const DriverSchema = new Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
   vehicle: {
-    year: { type: Number, required: true },
-    make: { type: String, required: true },
-    model: { type: String, required: true }
+    year: {
+      type: Number,
+      required: true
+    },
+    make: {
+      type: String,
+      required: true
+    },
+    model: {
+      type: String,
+      required: true
+    }
   },
   orders: [
     {
