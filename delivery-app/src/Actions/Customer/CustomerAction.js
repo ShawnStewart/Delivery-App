c9cb4a8d937acf8aea2bcb80aeb00b10e48421d4
Shawn Stewart <s.stewart0220@gmail.com>
2018-09-15 00:53:44 -0400

    Created login action/reducer

--- delivery-app/src/Actions/Customer/CustomerAction.js
+++ delivery-app/src/Actions/Customer/CustomerAction.js
@@ -1,4 +1,9 @@
-import { CUSTOMER_REGISTER_ERRORS, CUSTOMER_REGISTER_SUCCESS } from "../Types";
+import {
+  CUSTOMER_REGISTER_SUCCESS,
+  CUSTOMER_REGISTER_ERRORS,
+  CUSTOMER_LOGIN_SUCCESS,
+  CUSTOMER_LOGIN_ERRORS
+} from "../Types";
 import axios from "axios";
 
 // Register User
@@ -10,9 +15,24 @@ export const registerCustomer = (userData, history) => dispatch => {
       history.push("/login");
     })
     .catch(err => {
-      console.log(err);
       dispatch({
         type: CUSTOMER_REGISTER_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+
+// Login User
+export const loginCustomer = (userData, history) => dispatch => {
+  axios
+    .post("http://localhost:4000/api/customers/login", userData)
+    .then(res => {
+      dispatch({ type: CUSTOMER_LOGIN_SUCCESS });
+      localStorage.setItem("CustomerToken", res.data.token);
+    })
+    .catch(err => {
+      dispatch({
+        type: CUSTOMER_LOGIN_ERRORS,
         payload: err.response.data
       });
     });
--- delivery-app/src/Reducers/customerReducer.js
+++ delivery-app/src/Reducers/customerReducer.js
@@ -1,10 +1,13 @@
 import {
   CUSTOMER_REGISTER_ERRORS,
-  CUSTOMER_REGISTER_SUCCESS
+  CUSTOMER_REGISTER_SUCCESS,
+  CUSTOMER_LOGIN_ERRORS,
+  CUSTOMER_LOGIN_SUCCESS
 } from "../Actions/Types";
 
 const initialState = {
-  registerErrors: {}
+  registerErrors: {},
+  loginErrors: {}
 };
 
 export default function(state = initialState, action) {
@@ -13,6 +16,12 @@ export default function(state = initialState, action) {
       return { ...state, registerErrors: action.payload };
     case CUSTOMER_REGISTER_SUCCESS:
       return { ...state, registerErrors: {} };
+
+    case CUSTOMER_LOGIN_ERRORS:
+      return { ...state, loginErrors: action.payload };
+    case CUSTOMER_LOGIN_SUCCESS:
+      return { ...state, loginErrors: {} };
+
     default:
       return initialState;
   }
